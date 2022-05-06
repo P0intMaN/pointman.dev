d6acfae6bac5d2abf2f8b262f6938c0435eab59f
Pratheek U <pratheekastro3@gmail.com>
2022-05-06 15:33:55 +0530

    add tags, blogs routes

--- pointdev/routes.py
+++ pointdev/routes.py
@@ -5,6 +5,22 @@ from pointdev import app
 def home():
     return render_template('home.html')
 
+@app.route('/tags') 
+def tags():
+    return render_template('tags.html')
+
+@app.route('/blogs') 
+def blogs():
+    return render_template('blogs.html')
+
+@app.route('/join') 
+def join():
+    return render_template('home.html')
+
+@app.route('/store') 
+def store():
+    return render_template('home.html')
+
 @app.route('/page1') 
 def pages():
     return render_template('page1.html')
